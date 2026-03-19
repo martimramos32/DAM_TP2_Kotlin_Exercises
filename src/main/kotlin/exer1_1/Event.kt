7d217a77524937c5919edd550fbcf587b7496327
marti <martim.ramos2005@gmail.com>
2026-03-19 16:21:32 +0000

    Início da criação da sealed class Event que recebe as seguintes subclasses: Login, Purchase e Logout.

---
+++ src/main/kotlin/exer1_1/Event.kt
@@ -0,0 +1,7 @@
+package org.example.exer1_1
+
+sealed class Event {
+    class Login(val username: String, val timestamp: Long): Event()
+    class Purchase(val username: String, val amount: Double, val timestamp: Long): Event()
+    class Logout(val username: String, val timestamp: Long): Event()
+}
