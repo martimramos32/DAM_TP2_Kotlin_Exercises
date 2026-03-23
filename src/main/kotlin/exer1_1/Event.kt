542ab5e4975df2b5488a06348f936c760b7ef79c
marti <martim.ramos2005@gmail.com>
2026-03-23 21:12:51 +0000

    Criação da função totalSpent no ficheiro Event.kt

--- src/main/kotlin/exer1_1/Event.kt
+++ src/main/kotlin/exer1_1/Event.kt
@@ -15,3 +15,9 @@ fun List<Event>.filterByUser(username: String): List<Event> {
         }
     }
 }
+
+fun List<Event>.totalSpent(username: String): Double {
+    // O método filterIsIsntance retorna uma lista que contém todas as instancias de uma determinada classe. Neste caso aplicado retorna uma lista que contém apenas as instancias do evento Purchase
+    // Depois disso, usa-se o filter para encontrar apenas os eventos de compra do usuário em questão e por fim soma-se o valor de todas as compras para ter-se o valor total gasto (através do metodo sumOf)
+    return this.filterIsInstance<Event.Purchase>().filter { compra -> compra.username == username}.sumOf { compra -> compra.amount }
+}
