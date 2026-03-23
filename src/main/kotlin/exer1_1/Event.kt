512dad5ce4757b66662eb955a19bfff45d76a813
marti <martim.ramos2005@gmail.com>
2026-03-23 21:20:21 +0000

    Alterações na documentação das funções filterByUser e totalSpent.

--- src/main/kotlin/exer1_1/Event.kt
+++ src/main/kotlin/exer1_1/Event.kt
@@ -7,6 +7,9 @@ sealed class Event {
 }
 
 fun List<Event>.filterByUser(username: String): List<Event> {
+    //A função retorna uma lista com os eventos associados a um determinado utilizador
+    //Usa-se o metodo filter para iterar sobre a lista de eventos e verificar se o nome do utilizador está associado a algum dos vários eventos (Login, Purchase ou Logout).
+    //Se o nome do utilizador estiver associado a algum desses eventos, o evento é adicionado à lista de resultados que será retornada no final da função
     return this.filter { evento ->
         when(evento) {
             is Event.Login -> evento.username == username
@@ -17,7 +20,7 @@ fun List<Event>.filterByUser(username: String): List<Event> {
 }
 
 fun List<Event>.totalSpent(username: String): Double {
-    // O método filterIsIsntance retorna uma lista que contém todas as instancias de uma determinada classe. Neste caso aplicado retorna uma lista que contém apenas as instancias do evento Purchase
-    // Depois disso, usa-se o filter para encontrar apenas os eventos de compra do usuário em questão e por fim soma-se o valor de todas as compras para ter-se o valor total gasto (através do metodo sumOf)
+    //O metodo filterIsIsntance retorna uma lista que contém todas as instancias de uma determinada classe. Neste caso aplicado retorna uma lista que contém apenas as instancias do evento Purchase
+    //Depois disso, usa-se o filter para encontrar apenas os eventos de compra do usuário em questão e por fim soma-se o valor de todas as compras para ter-se o valor total gasto (através do metodo sumOf)
     return this.filterIsInstance<Event.Purchase>().filter { compra -> compra.username == username}.sumOf { compra -> compra.amount }
 }
