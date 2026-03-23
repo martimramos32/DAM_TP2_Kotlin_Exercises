1ad2063c758b6866b3349806ddafd7449662368a
marti <martim.ramos2005@gmail.com>
2026-03-23 17:12:51 +0000

    Criação da função filterByUser no ficheiro Event.kt

--- src/main/kotlin/exer1_1/Event.kt
+++ src/main/kotlin/exer1_1/Event.kt
@@ -5,3 +5,13 @@ sealed class Event {
     class Purchase(val username: String, val amount: Double, val timestamp: Long): Event()
     class Logout(val username: String, val timestamp: Long): Event()
 }
+
+fun List<Event>.filterByUser(username: String): List<Event> {
+    return this.filter { evento ->
+        when(evento) {
+            is Event.Login -> evento.username == username
+            is Event.Purchase -> evento.username == username
+            is Event.Logout -> evento.username == username
+        }
+    }
+}
